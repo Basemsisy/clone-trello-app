5a2ace14a45ecf7bb3c17872fce9c1b858a5ce95
basem elsisy <basemsisy@gmail.com>
2020-04-14 15:22:50 +0200

    initial code & project confegration

---
+++ src/store.js
@@ -0,0 +1,17 @@
+/*eslint-disable*/
+import Vue from "vue";
+import Vuex from "vuex";
+import defaultBoard from "./default-board";
+import { saveStatePlugin } from "./utils";
+
+Vue.use(Vuex);
+
+const board = JSON.parse(localStorage.getItem("board")) || defaultBoard;
+
+export default new Vuex.Store({
+  plugins: [saveStatePlugin],
+  state: {
+    board
+  },
+  mutations: {}
+});
